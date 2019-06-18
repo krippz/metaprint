8b43a565ad7facb5b86eda48e28a367b1430e591
Kristofer <kristofer@linnestjerna.se>
2019-06-18 23:12:42 +0200

    refactor read

--- src/metaprint/Readers/FileReader.cs
+++ src/metaprint/Readers/FileReader.cs
@@ -16,101 +16,18 @@ namespace Readers
     public class FileReader : IReader
     {
         private readonly IEnumerable<string> fileExtensions;
+
         public FileReader(Settings settings)
         {
             fileExtensions = settings.Extensions;
         }
-        private bool PathIsFile(string path)
-        {
-            try
-            {
-                return !File.GetAttributes(path).HasFlag(FileAttributes.Directory);
-            }
-            catch
-            {
-                return false;
-            }
-        }
-        private FileVersionInfo ReadFileVersionInfo(string path)
-        {
-            if (!PathIsFile(path))
-            {
-                return null;
-            }
-
-            try
-            {
-                return System.Diagnostics.FileVersionInfo.GetVersionInfo(path);
-            }
-            catch
-            {
-                return null;
-            }
-
-        }
-        private AssemblyName ReadAssemblyName(string path)
-        {
-            if (!PathIsFile(path))
-            {
-                return null;
-            }
-
-            try
-            {
-                return System.Reflection.AssemblyName.GetAssemblyName(path);
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        private PeFile ReadPeFile(string path)
-        {
-            if (!PathIsFile(path))
-            {
-                return null;
-            }
-            try
-            {
-                return new PeFile(path);
-            }
-            catch
-            {
-                return null;
-            }
-        }
-        private IEnumerable<string> GetFiles(IEnumerable<string> dirs)
-        {
-            return dirs.SelectMany(dir => Directory.GetFiles(dir, "*.*")
-                                                .Where(s => fileExtensions.Contains(Path.GetExtension(s))))
-                                                .ToList();
-        }
         public FileMetadata Read(string path)
         {
-            FileVersionInfo info = ReadFileVersionInfo(path);
-            AssemblyName assembly = ReadAssemblyName(path);
-            PeFile pefile = ReadPeFile(path);
-
             var meta = new FileMetadata();
-            if (info != null)
-            {
-                meta.FileName = Path.GetFileName(info.FileName.GetValueOrNotAvalible());
-                meta.InternalName = info.InternalName.GetValueOrNotAvalible();
-                meta.FileVersion = info.FileVersion.GetValueOrNotAvalible();
-                meta.CompanyName = info.CompanyName.GetValueOrNotAvalible();
-                meta.Copyright = info.LegalCopyright.GetValueOrNotAvalible();
-                meta.Trademark = info.LegalTrademarks.GetValueOrNotAvalible();
-                meta.ProductName = info.ProductName.GetValueOrNotAvalible();
-                meta.ProductVersion = info.ProductVersion.GetValueOrNotAvalible();
-            }
-            if (assembly != null)
-            {
-                meta.ProcessorArchitecture = Enum.GetName(typeof(ProcessorArchitecture), assembly.ProcessorArchitecture);
-            }
-            if (pefile != null)
-            {
-                meta.Bitness = pefile.Is64Bit ? "x64" : "x32";
-            }
+
+            ReadFileAs<FileVersionInfo>(path, ReadFileVersionInfo, meta);
+            ReadFileAs<AssemblyName>(path, ReadAssemblyName, meta);
+            ReadFileAs<PeFile>(path, ReadPeFile, meta);
 
             return meta;
         }
@@ -130,6 +47,76 @@ namespace Readers
             }
 
             return Enumerable.Empty<FileMetadata>();
+        }
+        private IEnumerable<string> GetFiles(IEnumerable<string> dirs)
+        {
+            return dirs.SelectMany(dir => Directory.GetFiles(dir, "*.*")
+                                                .Where(s => fileExtensions.Contains(Path.GetExtension(s))))
+                                                .ToList();
+        }
+        private bool PathIsFile(string path)
+        {
+            try
+            {
+                return !File.GetAttributes(path).HasFlag(FileAttributes.Directory);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private T Execute<T>(Func<T> function)
+        {
+            try
+            {
+                return function();
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+        private FileVersionInfo ReadFileVersionInfo(string path)
+        {
+            return PathIsFile(path) ? Execute(() => System.Diagnostics.FileVersionInfo.GetVersionInfo(path)) : null;
+        }
+        private AssemblyName ReadAssemblyName(string path)
+        {
+            return PathIsFile(path) ? Execute(() => System.Reflection.AssemblyName.GetAssemblyName(path)) : null;
+        }
+        private PeFile ReadPeFile(string path)
+        {
+            return PathIsFile(path) ? Execute(() => new PeFile(path)) : null;
+        }
+        private void ReadFileAs<T>(string path, Func<string, T> reader, FileMetadata data)
+        {
+            T result = reader(path);
+            if (result == null)
+            {
+                return;
+            }
+
+            switch (result)
+            {
+                case FileVersionInfo info:
+                    data.FileName = Path.GetFileName(info.FileName.GetValueOrNotAvalible());
+                    data.InternalName = info.InternalName.GetValueOrNotAvalible();
+                    data.FileVersion = info.FileVersion.GetValueOrNotAvalible();
+                    data.CompanyName = info.CompanyName.GetValueOrNotAvalible();
+                    data.Copyright = info.LegalCopyright.GetValueOrNotAvalible();
+                    data.Trademark = info.LegalTrademarks.GetValueOrNotAvalible();
+                    data.ProductName = info.ProductName.GetValueOrNotAvalible();
+                    data.ProductVersion = info.ProductVersion.GetValueOrNotAvalible();
+                    break;
+                case AssemblyName asm:
+                    data.ProcessorArchitecture = Enum.GetName(typeof(ProcessorArchitecture), asm.ProcessorArchitecture);
+                    break;
+                case PeFile pe:
+                    data.Bitness = pe.Is64Bit ? "x64" : "x32";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
